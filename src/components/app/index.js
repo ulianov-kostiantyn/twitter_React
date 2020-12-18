87895a1c6ae7fa26905cd1a8d87995f0cc311da1
Ulik <example@gmail.com>
2020-12-18 17:47:28 +0100

    front-end realised

---
+++ src/components/app/index.js
@@ -0,0 +1,5 @@
+import App from './app'
+import '../../css/index.css'
+
+
+export default App;
---
+++ src/components/post-list/post-list.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import '../../css/post-list.css'
+
+
+import PostListItem from '../post-list-item';
+
+const PostList = ({posts, onDelete, onToggleImportant, onToggleLiked}) => {
+
+    const elements = posts.map((item) => {
+         if ( typeof item === 'object' && isEmpty(item) ){ 
+            const {id, ...itemProps} = item;
+            return (
+                <li key={id} className="list-group-item">
+                    <PostListItem 
+                        {...itemProps}
+                        onDelete={() => onDelete(id)}
+                        onToggleImportant={() => onToggleImportant(id)}
+                        onToggleLiked={() => onToggleLiked(id)}/>
+                </li>
+            )
+        }
+    })
+
+    function isEmpty(obj) {
+        for(let key in obj)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    return (
+        <ul className="app-list list-group">
+            {elements}
+        </ul>
+    )
+}
+
+export default PostList;
